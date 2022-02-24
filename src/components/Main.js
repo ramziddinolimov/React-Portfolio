48ece017a12c7e18436b826657c9d6422a35dfa4
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-02-25 00:02:47 +0500

    contact mainjs

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 import LogoComponent from '../subComponents/LogoComponent'
 import PowerButton from '../subComponents/PowerButton'
@@ -24,6 +25,15 @@ const Container = styled.div`
   padding: 2rem;
 `
 
+const Contact = styled(NavLink)`
+  color: ${props => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`
+
 function Main() {
   return (
     <MainContainer>
@@ -31,6 +41,12 @@ function Main() {
           <PowerButton/>
           <LogoComponent/>
           <SocialIcons/>
+
+          <Contact target="_blank" to={{pathname:"mailto:ramizmafiya@gmail.com"}}>
+            <h3>
+              Say hi...
+            </h3>
+          </Contact>
         </Container>
     </MainContainer>
   )
