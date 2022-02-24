677dc343148f388264e98abc38fa5f56342e40d3
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-02-24 22:37:54 +0500

    first child power button

--- src/subComponents/PowerButton.js
+++ src/subComponents/PowerButton.js
@@ -15,17 +15,23 @@ background-color: #FCF6F4;
   border-radius: 50%;
   border: 1px solid #000;
   width: 2.5rem;
-  height: 2.5rem;
+  height: 2.3rem;
 
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 3;
   cursor: pointer;
+  transition: .3s;
 
   &:hover {
     background-color: rgba(0,255,0,0.4);
     box-shadow: 0 0 8px 6px rgba(0,255,0,0.2);
+  }
+
+  &>*:first-child{
+    text-decoration: none;
+    color: inherit;
   }
 `
 
