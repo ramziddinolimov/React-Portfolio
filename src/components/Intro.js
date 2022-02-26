543c17bf358035e9d2191159cbe8a843933b9860
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-02-26 22:15:07 +0500

    intro continued

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,6 +8,7 @@ const Box = styled.div`
     top: 50%;
     transform: translate(-50%, -50%);
     width: 50vw;
+    height: 40vh;
     display: flex;
     background-repeat: no-repeat;
     background-size: 100% 2px;
@@ -16,10 +17,12 @@ const Box = styled.div`
         ${props => props.theme.body} 50%,
         ${props => props.theme.text} 50%) bottom,
         linear-gradient(
+            to right,
             ${props => props.theme.body} 50%,
             ${props => props.theme.text} 50%) top;
-    border-left: 2px solid ${props => props.theme.body}
-    border-right: 2px solid ${props => props.theme.text}
+    border-left: 2px solid ${props => props.theme.body};
+    border-right: 2px solid ${props => props.theme.text};
+    z-index:1;
         
 `
 
