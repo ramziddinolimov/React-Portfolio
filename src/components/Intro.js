4d2231799f2e3a8d8785d6e6cf8e64202a7fc9fd
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-02-26 23:48:23 +0500

    intro img continued

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,11 +9,9 @@ const Box = styled.div`
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
-    width: 50vw;
-    height: 40vh;
+    width: 65vw;
+    height: 55vh;
     display: flex;
-    background-repeat: no-repeat;
-    background-size: 100% 2px;
     background: linear-gradient(
         to right,
         ${props => props.theme.body} 50%,
@@ -22,10 +20,43 @@ const Box = styled.div`
             to right,
             ${props => props.theme.body} 50%,
             ${props => props.theme.text} 50%) top;
+    background-repeat: no-repeat;
+    background-size: 100% 2px;
     border-left: 2px solid ${props => props.theme.body};
     border-right: 2px solid ${props => props.theme.text};
     z-index:1;
         
+`
+
+const SubBox = styled.div`
+    width: 50%;
+    position: relative;
+    display: flex;
+
+    .pic {
+    position: absolute;
+    bottom: -25%;
+    left: 50%;
+    transform: translate(-49.9%, 0%);
+    width: 100%;
+    height: auto;
+    }
+`
+
+const Text = styled.div`
+font-size: calc(1em + 1.5vw);
+color: ${props => props.theme.body};
+padding: 2rem;
+cursor: pointer;
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+
+&>*:last-child{
+    color: ${props => `rgba(${props.theme.bodyRgba}, 0.6)`};
+    font-size: calc(0.5rem + 1.5vw);
+  font-weight: 300;
+}
 `
 
 const Intro = () => {
@@ -40,7 +71,7 @@ const Intro = () => {
         </SubBox>
         <SubBox>
             <div>
-                <img className='pic' src={Me} alt='Profile img'>
+                <img className='pic' src={Me} alt='Profile img'/>
             </div>
         </SubBox>
     </Box>
