e7f529260af4ef4332299fe6944a562ec3f4e448
ramziddinolimovv <ramizmafiya@gmail.com>
2022-04-12 20:08:10 +0500

    blog page started

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+import img from "../assets/Images/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg"
+
+
+const MainContainer = styled.div`
+  background-image: url(${img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+  width: 100vw;
+  height: 100vh;
+
+`
 
 function BlogPage() {
   return (
-    <div>
-        BlogPage Component/Page
-    </div>
+    <MainContainer>
+        
+    </MainContainer>
   )
 }
 
